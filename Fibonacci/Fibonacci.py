021ea702045bf61162b7233f12c451a9515cafcf
Sabboshachi Sarkar <55670542+Sabboshachi-Sarkar@users.noreply.github.com>
2020-02-18 00:24:30 +0600

    initial commit for fibo

---
+++ Fibonacci/Fibonacci.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+fib_1 = 0
+fib_2 = 1
+
+print(fib_1)
+
+while fib_2 <= 100:
+    print (fib_2)
+    fib_1 , fib_2 = fib_2 , fib_1 + fib_2
+
+
+# In[ ]:
+
+
+
+
