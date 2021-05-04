9c2e3b74b07d2e464f92a712f5626036563fc7cf
Sabboshachi Sarkar <55670542+Sabboshachi-Sarkar@users.noreply.github.com>
2021-05-04 16:19:27 +0600

    list tuple conversion

---
+++ fromBook/03.Automate_the_Boring_Stuff_with_Python/Part_1/14.list&TupleConversion.py
@@ -0,0 +1,16 @@
+name = ["susmita", "dey", "tip"]
+print(type(name))
+name = tuple()
+print(type(name))
+
+##########################################################
+
+name2 = ("sabboshachi" , "sarkar", "ankan")
+print(type(name2))
+name2 = list()
+print(type(name2))
+
+##########################################################
+
+stringTOList =  list("hello")
+print(stringTOList)
