468dace3ed0b07a445b4bd1b947002a3ffe5f8a9
Sabboshachi Sarkar <55670542+Sabboshachi-Sarkar@users.noreply.github.com>
2021-05-04 16:18:23 +0600

    list

---
+++ fromBook/03.Automate_the_Boring_Stuff_with_Python/Part_1/12.somemoreWorkWIthLists-3.py
@@ -0,0 +1,10 @@
+cat = ["fat", "balck", "loud"]
+
+size, color, dispositon = cat
+
+## cat = size, color, dispositon                           << Wrong command >>
+
+print(size)
+print(color)
+print(dispositon)
+
