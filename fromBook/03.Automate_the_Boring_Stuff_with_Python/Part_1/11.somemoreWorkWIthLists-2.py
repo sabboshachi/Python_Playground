57a2dec81ccb949dfc6afbbf2aa505c33a26efd9
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2021-06-02 23:28:28 +0600

    Update

--- fromBook/03.Automate_the_Boring_Stuff_with_Python/Part_1/11.somemoreWorkWIthLists-2.py
+++ fromBook/03.Automate_the_Boring_Stuff_with_Python/Part_1/11.somemoreWorkWIthLists-2.py
@@ -1,5 +1,5 @@
-supplies = ["pens", "pencel", "binders", "notebook"]
+supplies = ["pens", "pencel", "binders", "notebook"] #calling the list
 
-for item in range(len(supplies)):
+for item in range(len(supplies)): #working with for loop
     print("Index " + str(item+1) + " in supplies is: " + supplies[item].title() + '.')
 
