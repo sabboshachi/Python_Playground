edcee28c2af1fde2ada481c532da64a0b6f4c1bf
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2021-06-02 23:27:42 +0600

    Update

--- fromBook/03.Automate_the_Boring_Stuff_with_Python/Part_1/19.dictionaryDataType.py
+++ fromBook/03.Automate_the_Boring_Stuff_with_Python/Part_1/19.dictionaryDataType.py
@@ -1,10 +1,10 @@
-birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'} #making a list
 
 
 while True:
     print("Enter a name: (blank to quit)")
     name = input()
-    if name == "":
+    if name == "": #conditional logic
         break
     if name in birthdays.keys():
         print(birthdays[name] + " is the birthday of " + name)
