c44b72469c13d73ac76559c15a7dceaaec609ead
sabboshachi <sabboshachi.ruet@gmail.com>
2021-08-09 11:21:49 +0600

    code update

--- regularPractice/Turtle_Square.py
+++ regularPractice/Turtle_Square.py
@@ -1,28 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[18]:
-
-
-import turtle 
+import turtle
 turtle.shape("turtle")
 turtle.speed(2)
 
-for length in range(50,101,10):
+for length in range(50,500,10):
     for i in range(4):
         turtle.forward(length)
         turtle.left(90)
 turtle.exitonclick()
-        
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
