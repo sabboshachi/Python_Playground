9f88fe0f6904ed8c0075aaffc9e1dbd01a5f7e23
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2023-01-22 19:58:24 +0600

    Modifying Attributes with a condition

--- regularPractice/03.ModifyingAttributesThroughMethod.py
+++ regularPractice/03.ModifyingAttributesThroughMethod.py
@@ -6,7 +6,7 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
-        self.odometer_reading = 0
+        self.odometer_reading = 150
 
     def get_descriptive_name(self):
         """Return a neatly formatted descritive name."""
@@ -18,15 +18,21 @@ class Car():
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
     def update_odometer(self, mileage):
-        """Set the odometer reading to the given value."""
-        self.odometer_reading = mileage
+        # """Set the odometer reading to the given value."""
+        # self.odometer_reading = mileage
 
+        """Set the odometer reading to the given value. Reject the change if it attempts to roll the odometer back."""
+
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
 
 
 my_new_car = Car("BMW", "a4", 2016)
 print(my_new_car.get_descriptive_name())
 
-my_new_car.update_odometer(150)
+my_new_car.update_odometer(200)
 my_new_car.read_odometer()
 
 
