c30971bdc086707f7bf25bb327286b1fc0002af5
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2023-01-30 23:14:12 +0600

    Create imageHighPassFilter.py

---
+++ regularPractice/imageHighPassFilter.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Load the image
+img = cv2.imread("image.jpg", cv2.IMREAD_GRAYSCALE)
+
+# Create a high-pass filter
+kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+
+# Apply the filter to the image
+filtered_img = cv2.filter2D(img, -1, kernel)
+
+# Plot the original and filtered images
+plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.title("Original Image"), plt.xticks([]), plt.yticks([])
+
+plt.subplot(122), plt.imshow(filtered_img, cmap='gray')
+plt.title("Filtered Image"), plt.xticks([]), plt.yticks([])
+
+plt.show()
